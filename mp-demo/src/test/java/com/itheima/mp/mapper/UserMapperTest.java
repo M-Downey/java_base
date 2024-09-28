b35f3b7dfebea827c2bdfb0f3c6272dc05eb9692
tony <923940358@qq.com>
2024-09-28 23:32:32 +0800

    mp分页插件测试

--- mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,6 +1,8 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.mp.domain.po.User;
+import com.itheima.mp.service.IUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +17,8 @@ class UserMapperTest {
 
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private IUserService userService;
 
     @Test
     void testInsert() {
@@ -60,5 +64,14 @@ class UserMapperTest {
     @Test
     void testDeleteUser() {
         userMapper.deleteUser(5L);
+    }
+
+    @Test
+    void testPageQuery() {
+        Page<User> page = userService.page(new Page<>(1, 2));
+        System.out.println(page.getTotal());
+        System.out.println(page.getPages());
+        List<User> users = page.getRecords();
+        users.forEach(System.out::println);
     }
 }
